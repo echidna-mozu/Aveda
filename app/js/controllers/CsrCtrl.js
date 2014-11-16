a4763330bd94391091bd7f3b556dac597dcfdc4a
Steve Davis <sdavis@four51.com>
2014-11-16 08:24:11 -0600

    quick order functionality

--- app/js/controllers/CsrCtrl.js
+++ app/js/controllers/CsrCtrl.js
@@ -1,5 +1,42 @@
-four51.app.controller('CsrCtrl', ['$scope', 'Order', 'User', 'Product',
-	function ($scope, Order, User, Product) {
-		$scope.loading = false;
+four51.app.controller('CsrCtrl', ['$scope', 'Order', 'User', 'Product', 'ProductDisplayService',
+	function ($scope, Order, User, Product, ProductDisplayService) {
+		$scope.LineItem = {};
+
+		$scope.getEligibleProducts = function(input) {
+			return Product.quick(input).then(function(products) {
+				return products;
+			});
+		};
+
+		$scope.$watch('LineItem.Product.InteropID', function(id) {
+			if (!id) return;
+			ProductDisplayService.getProductAndVariant(id, null, function(product) {
+				$scope.LineItem.Product = product.product;
+				$scope.LineItem.Variant = product.variant;
+				ProductDisplayService.setNewLineItemScope($scope);
+				ProductDisplayService.setProductViewScope($scope);
+			});
+		});
+
+		$scope.addToOrder = function() {
+			$scope.currentOrder = $scope.currentOrder || {};
+			$scope.currentOrder.LineItems = $scope.currentOrder.LineItems || [];
+
+			$scope.currentOrder.LineItems.push($scope.LineItem);
+			Order.save($scope.currentOrder,
+				function(o){
+					$scope.currentOrder = o;
+					$scope.user.CurrentOrderID = o.ID;
+					User.save($scope.user, function(user){
+						$scope.user = user;
+					});
+					$scope.quickAddMessage = "Item added to cart";
+					$scope.LineItem = {};
+				},
+				function(ex) {
+					$scope.addToOrderError = ex.Message;
+				}
+			);
+		};
 	}
 ]);
